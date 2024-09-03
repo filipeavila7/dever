4c34e4a49bb37f94b17da1e48d29344757b3b789
Joao <joaopedroguilicru@gmail.com>
2024-09-03 11:54:03 -0300

    atv

---
+++ atvds/ativi04.py
@@ -0,0 +1,10 @@
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro numero: "))
+# declarando variaveis
+if num1 > num2:
+    print('o primeiro numero e maior')
+elif num1 == num2:
+    print('os numeros são iguais')
+else:
+    print('o primeiro numero e menor')
+# vendo se o numero e maior ou menor
---
+++ atvds/ativi06.py
@@ -0,0 +1,16 @@
+lista = []
+num1 = int(input('insira um numero: '))
+lista.append(num1)
+num2 = int(input('insira um numero: '))
+lista.append(num2)
+num3 = int(input('insira um numero: '))
+lista.append(num3)
+num4 = int(input('insira um numero: '))
+lista.append(num4)
+lista.sort()
+print(lista)
+# lista das variaveis
+print('o maior numero é', lista [3])
+print('o menor numero e', lista [0])
+# qual e o numero maior ou menor
+
